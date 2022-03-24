a2b808c8592e3139e2badb526c2bbfe5cc830b4d
Wang JL <jimmymall@live.com>
2022-03-24 17:01:51 +0800

    Update run.py

--- zhihu/run.py
+++ zhihu/run.py
@@ -3,6 +3,7 @@ import requests
 
 #发出请求
 url= 'https://www.economist.com/'
+url="https://rawcdn.githack.com/MarioniLab/miloR/3646391023f600bae00efd9d940b888503d7a536/docs/articles/milo_demo.html"
 
 respose=requests.get(url)
 #print(respose.status_code)# 响应的状态码 200
@@ -10,7 +11,7 @@ respose=requests.get(url)
 #print(respose.text)  #返回文本内容
 
 # write to file
-with open("index.html",'w') as f:
+with open("index2.html",'w') as f:
     f.write(respose.text)
 
 print("end")
