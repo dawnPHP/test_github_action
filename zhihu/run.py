a7b21abbe1f0c878b6724a58635187dcd63eb378
Wang JL <jimmymall@live.com>
2022-04-15 16:21:50 +0800

    update URL

--- zhihu/run.py
+++ zhihu/run.py
@@ -3,7 +3,7 @@ import requests
 
 #发出请求
 url= 'https://www.economist.com/'
-url="https://rawcdn.githack.com/MarioniLab/miloR/3646391023f600bae00efd9d940b888503d7a536/docs/articles/milo_demo.html"
+url="https://www.economist.com/printedition"
 
 respose=requests.get(url)
 #print(respose.status_code)# 响应的状态码 200
